78ba4d529d2d3a627b6dd69d02c69a2ef82c4bf1
Templar <carlegos101@gmail.com>
2024-04-14 04:44:20 -0500

    Enemies can kick boxes now

--- Assets/_Templar/Scripts/PropKick.cs
+++ Assets/_Templar/Scripts/PropKick.cs
@@ -2,42 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PropKick : TriggerDetector
+public class PropKick : MonoBehaviour
 {
-    public EnemySpawner enemySpawner;
+    public Enemy enemy;
+    public Rigidbody lastTouchRb;
     private void OnValidate()
     {
-        if (enemySpawner == null) enemySpawner = FindObjectOfType<EnemySpawner>();
+        if (enemy == null) enemy = GetComponent<Enemy>();
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player"))
+        if(enemy.CurrentState != Enemy.EnemyState.Idle && collision.gameObject.CompareTag("Prop"))
         {
-            TargetTagIsWithinRange = true;
-            OnPlayerTriggerEnter.Invoke();
-        }
-        if (other.CompareTag("Player"))
-        {
-            TargetTagIsWithinRange = true;
-        }
-    }
-    private void Update()
-    {
-        if (TargetTagIsWithinRange && Input.GetButtonDown("Jump"))
-        {
-            OnPlayerInteract.Invoke();
-            Debug.Log("player kicked Me!");
-            if (AudioSourcePrefabToSpawnInOnInteract != null) Instantiate(AudioSourcePrefabToSpawnInOnInteract, transform.position, Quaternion.identity);
-            if (SpawnMeOnInteract != null) Instantiate(SpawnMeOnInteract, transform.position, Quaternion.identity);
-            if (DestoryMeOnInteract != null) Destroy(DestoryMeOnInteract);
+            Rigidbody propRb = null;
+            if(collision.gameObject.TryGetComponent<Rigidbody>(out propRb))
+            {
+                PushAway(propRb);
+            }
         }
     }
-    public void KickAwayFromPlayer()
+    private void OnCollisionStay(Collision collision)
     {
-
+        if (enemy.CurrentState != Enemy.EnemyState.Idle && collision.gameObject.CompareTag("Prop"))
+        {
+            Rigidbody propRb = null;
+            if (collision.gameObject.TryGetComponent<Rigidbody>(out propRb))
+            {
+                PushAway(propRb);
+            }
+        }
     }
-    public void KickAwayFromEnemy()
+    public void PushAway(Rigidbody propRb)
     {
-
+        lastTouchRb = propRb;
+        Debug.Log("Enemy Pushed Prop");
+        Vector3 pushDirection = (propRb.transform.position - transform.position).normalized * enemy.PushForce*0.2f;
+        propRb.AddForce(pushDirection, ForceMode.Impulse);
     }
 }
